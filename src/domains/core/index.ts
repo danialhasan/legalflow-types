4fb75abc2d5b18d9b2b775bbe14aae603479029d
Danial Hasan <danialhasan14@gmail.com>
2025-04-08 01:29:26 -0400

    update types

--- src/domains/core/index.ts
+++ src/domains/core/index.ts
@@ -1,6 +1,6 @@
 /**
  * Core Domain Types
- * 
+ *
  * Contains type definitions for the core schema tables and related types.
  * These types represent fundamental system entities like users and jobs.
  */
@@ -13,16 +13,16 @@ export type CoreSchema = Database['core']['Tables'];
 export type ErrorLog = CoreSchema['error_logs']['Row'];
 export type Job = CoreSchema['jobs']['Row'];
 export type UserRanking = CoreSchema['user_rankings']['Row'];
-export type User = CoreSchema['users']['Row'];
+export type User = CoreSchema['user_metadata']['Row'];
 
 // Table type exports - Insert types
 export type ErrorLogInsert = CoreSchema['error_logs']['Insert'];
 export type JobInsert = CoreSchema['jobs']['Insert'];
 export type UserRankingInsert = CoreSchema['user_rankings']['Insert'];
-export type UserInsert = CoreSchema['users']['Insert'];
+export type UserInsert = CoreSchema['user_metadata']['Insert'];
 
 // Table type exports - Update types
 export type ErrorLogUpdate = CoreSchema['error_logs']['Update'];
 export type JobUpdate = CoreSchema['jobs']['Update'];
 export type UserRankingUpdate = CoreSchema['user_rankings']['Update'];
-export type UserUpdate = CoreSchema['users']['Update']; 
+export type UserUpdate = CoreSchema['user_metadata']['Update'];
